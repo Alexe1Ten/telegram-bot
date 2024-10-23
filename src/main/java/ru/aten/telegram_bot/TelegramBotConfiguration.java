cb138795d88ddd2d5e4d323ca4862216b06b315d
AlexeyTen <aten9180929937@gmail.com>
2024-10-23 12:56:59 +0300

    A simple echo bot has been created

--- src/main/java/ru/aten/telegram_bot/TelegramBotConfiguration.java
+++ src/main/java/ru/aten/telegram_bot/TelegramBotConfiguration.java
@@ -1,8 +1,32 @@
 package ru.aten.telegram_bot;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import lombok.SneakyThrows;
 
 @Configuration
 public class TelegramBotConfiguration {
 
+    @Bean
+    @SneakyThrows
+    public TelegramBot telegramBot(
+        @Value("${bot.token}") String botToken,
+        TelegramBotsApi telegramBotsApi
+    ) {
+        var botOptions = new DefaultBotOptions();
+        var bot =  new TelegramBot(botOptions, botToken);
+        telegramBotsApi.registerBot(bot);
+        return bot;
+    }
+    
+    @Bean
+    @SneakyThrows
+    public TelegramBotsApi telegramBotApi() {
+        return new TelegramBotsApi(DefaultBotSession.class);
+    }
 }
